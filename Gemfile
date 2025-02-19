126d42e8faf9edd89f5b36fa19d2e4646c42092d
Abe G <abenbg12@gmail.com>
2025-02-19 14:45:14 -0500

    Need to edit

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@ source "https://rubygems.org"
 
 ruby "3.3.5"
 
+#simple form
+gem "simple_form"
+
+#faker gem
+gem "faker"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.5", ">= 7.1.5.1"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+cuisines = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant = Restaurant.create!(name: Faker::Name.name,
+    address: Faker::Address.street_address, phone_number: Faker::Number.leading_zero_number(digits: 10),
+  category: cuisines.sample
+  )
+  p "#{restaurant.name} is #{restaurant.category}"
+end
